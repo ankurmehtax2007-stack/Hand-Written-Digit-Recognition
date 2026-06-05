22e1d37b0e55543c9eec28ab47c58322c1b3af92
Ankur Mehta <ankurmehta@Ankurs-MacBook-Air.local>
2026-06-05 11:22:14 +0530

    Ready for deployment

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": "*"
+        }
+    }
+)
 
 from routes.predict import predict_bp
 from routes.history import history_bp
@@ -22,6 +29,15 @@ def home():
         "status":"running"
     }
 
+import os
+
 if __name__ == "__main__":
+<<<<<<< HEAD
     app.run(host="0.0.0.0", port=5000
            )
+=======
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
+>>>>>>> 3f03f9f (Ready for deployment)
